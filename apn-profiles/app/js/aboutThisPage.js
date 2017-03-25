3c4bfbe2eda93341d784acc8980a3e59d98256ca
WataruSuzuki <wataru0406@gmail.com>
2017-03-25 13:30:00 +0900

    Delete old apn-profiles

--- apn-profiles/app/js/aboutThisPage.js
+++
@@ -1,8 +0,0 @@
-function launchApp(){
-    var url = "jchankchanapnassistant://";
-    var ref = window.open(url, '_blank', 'location=yes');
-}
-
-function loadProjectAPNAssistant(){
-    var ref = window.open('https://watarusuzuki.github.io/APNAssistant/');
-}
